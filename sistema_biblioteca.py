4cab275319dba25f3c2e69a4f01fb8293ec80dfe
Rodrigo Donoso <rygo.dc@gmail.com>
2024-10-08 01:21:03 +0200

    libro

--- sistema_biblioteca.py
+++ sistema_biblioteca.py
@@ -3,3 +3,21 @@ class Libro:
         self.__id = id
         self.__titulo = titulo
         self.__autor = autor
+        self.__disponible = True
+
+    def get_id(self):
+        return self.__id
+
+    def get_titulo(self):
+        return self.__titulo
+
+    def get_autor(self):
+        return self.__autor
+    
+    def marcar_prestado(self):
+        self.__disponible = False
+
+    def marcar_disponible(self):
+        self.__disponible = True
+
+
