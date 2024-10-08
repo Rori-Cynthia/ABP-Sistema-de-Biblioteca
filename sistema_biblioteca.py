42f77ceb4f18f86d82fd70df929a6ee97665d403
Rodrigo Donoso <rygo.dc@gmail.com>
2024-10-08 02:00:54 +0200

    avance

--- sistema_biblioteca.py
+++ sistema_biblioteca.py
@@ -20,4 +20,35 @@ class Libro:
     def marcar_disponible(self):
         self.__disponible = True
 
+class Prestamo:
+    def __init__(self):
+        self.prestamos = []
+
+    def registrar_prestamo(self, libro, usuario):
+        if libro.get_disponible():
+            fecha_prestamo = input("Ingrese la fecha de prestamo: ")
+            self.prestamos.append(f"Usuario: {usuario}, Libro: {libro}, Fecha de prestamo: {fecha_prestamo}")
+            libro.marcar_prestado()
+            print("Prestamo registrado")
+        else:
+            print("El libro ya se encuentra prestado")
+
+    def registrar_devolucion(self, libro):
+        for prestamo in self.prestamos:
+            if prestamo["Libro"] == libro:
+                fecha_devolucion = input("Ingrese la fecha de devolución: ")
+                prestamo["Fecha de devolución"] = fecha_devolucion
+                libro.marcar_disponible()
+                print("Devolucion registrada")
+                break
+        else:
+            print("El libro no se encuentra prestado")
+
+    libro1 = Libro(1, "El principito", "Antonio Machado")
+    libro2 = Libro(2, "Cien años de soledad", "Gabriel Garcia Marquez")
+    libro3 = Libro(3, "El señor de los anillos", "JRR Tolkien")
+    libro4 = Libro(4, "El resplandor", "Stephen King")
+    libro5 = Libro(5, "Harry Potter y la piedra filosofal", "J.K. Rowling")
+
+    
 
